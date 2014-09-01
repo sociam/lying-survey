e27f94a8665b385dfedcd5de215493e1bb145acb
electronic Max <electronic@gmail.com>
2014-09-01 17:31:37 +0100

    stage time

--- www/js/index.js
+++ www/js/index.js
@@ -8,21 +8,29 @@ angular.module('survey', ['ui.router', 'ngAnimate'])
 			url:'/start',
 			templateUrl:'tmpl/start.html',
 			controller:function($scope, $state, utils) {
-				var u = utils, sa = function(f) { utils.safeApply($scope, f); };
-			    window.ss = $scope;
+				
+				var u = utils, 
+					sa = function(f) { utils.safeApply($scope, f); },
+					stageTime = 0,
+					resetTime = function() { stageTime = (new Date()).valueOf(); };
+
 			    $scope.stage = 0;
 			    $scope.startSurvey = function() { 
 					$scope.userid = u.guid();
 					$scope.stage = 1;
+					resetTime();
 			    };
 			    $scope.next = function(qid, data) { 
-			    	console.log('next >> ');
 			    	$scope.stage++;
+			    	var elapsed = (new Date()).valueOf() - stageTime;
+			    	// add elapsed here
+			    	resetTime();
 			    };
 			    $scope.prev = function() { 
 			    	console.log('<< prev ');
 			    	$scope.stage--;
 			    };
+   			    window.ss = $scope;
 			}
 		});
 	}).controller('main', function($scope, utils) { 
