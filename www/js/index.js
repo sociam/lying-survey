0307a69ee06726542e2303ef6968458865ace21b
Dr. Daniel A Smith <daniel@danielsmith.eu>
2014-09-01 16:54:47 +0100

    Submission works

--- www/js/index.js
+++ www/js/index.js
@@ -10,14 +10,29 @@ angular.module('survey', ['ui.router', 'ngAnimate'])
 			controller:function($scope, $state, utils) {
 				var u = utils, sa = function(f) { utils.safeApply($scope, f); };
 			    window.ss = $scope;
+
+                var submitAnswers = function (uuid, question_id, answer, misc) {
+                    answer = JSON.stringify(answer);
+                    misc = JSON.stringify(misc);
+                    jQuery.ajax({
+                        url: "http://localhost:3000/submit", // customise this
+                        type: "POST",
+                        data: {"uuid": uuid, "question_id": question_id, "answer": answer, "misc": misc},
+                        success: function () { console.log("submitted successfully"); },
+                        error: function (jqXHR, textStatus, errorThrown) { console.log("submission failure", textStatus, errorThrown); },
+                    });
+                };
+                window.sa = submitAnswers;
+
 			    $scope.startSurvey = function() { 
 					$scope.userid = u.guid();
 					$scope.stage = 1;
 					// advance!
 			    };
-			    $scope.next = function(qid, data) { 
-			    	console.log('next >> ');
+			    $scope.next = function(qid, data, misc) { 
+			    	console.log('next >> ', qid, data, misc);
 			    	$scope.stage++;
+                    submitAnswers($scope.userid, qid, data, misc);
 			    };
 			    $scope.prev = function() { 
 			    	console.log('<< prev ');
